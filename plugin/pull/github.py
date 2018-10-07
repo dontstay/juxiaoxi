f23e2e6958d7c5298b2e2b6f79d2de29f602c9ca
dontstay <578779997@qq.com>
2018-10-07 13:04:21 +0800

    base version
    1.pull.github
    2.push.blog

--- plugin/pull/github.py
+++ plugin/pull/github.py
@@ -61,7 +61,7 @@ class GitHubPlugin(base.PullPlugin):
             for key in cont.all_keys:
                 if key == 'name':
                     result += "# %s \n" % t.get(key, '')
-                elif key in ['url']:
+                elif key in ['html_url']:
                     result += "- **%s**: %s \n" % (key, t.get(key, ''))
                 else:
                     result += "- **%s**: `%s` \n" % (key, t.get(key, ''))
