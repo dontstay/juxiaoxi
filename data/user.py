d7b2760cff595b87db836a2dc4e87e9ae611cfda
dontstay <578779997@qq.com>
2018-12-07 17:51:00 +0800

    open github star python projects
    add gitignore

--- data/user.py
+++ data/user.py
@@ -3,7 +3,7 @@
 users = {
     "bobby": {
       "pull": {
-          #"github": ["python_stared"],
+          "github": ["python_stared"],
           "rss": ["cnblog_picked", "cnblog_python", "cnblog_linux"]
       },
       "push": "blog"
